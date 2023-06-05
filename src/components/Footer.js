da911e15bb96758480a53ec6a2048ec2a2f7d2c2
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-06-05 21:02:34 +0000

    created a footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = ({ customStyle }) => {
+  return (
+    <div className={`page-footer ${customStyle}`}>Built by @mariusniemet05</div>
+  );
+};
+
+export default Footer;
