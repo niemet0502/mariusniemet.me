1cea580007f5c6e5964be931b8b31250e25e8873
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-06-18 14:00:37 +0000

    created an array for the books list

---
+++ src/data/booksList.js
@@ -0,0 +1,23 @@
+export const books = [
+  {
+    id: 1,
+    title: "Harry Potter Series",
+    image: "harrypotter.jpg",
+    stars: null,
+    link: "https://www.amazon.fr/Alchemist-Paulo-Coelho/dp/0008283648/ref=sr_1_1?keywords=the+alchemist&qid=1686241491&sprefix=the+alceh%2Caps%2C149&sr=8-1",
+  },
+  {
+    id: 2,
+    title: "The alchemist",
+    image: "thealchemist.jpg",
+    stars: 1,
+    link: "https://www.amazon.fr/Alchemist-Paulo-Coelho/dp/0008283648/ref=sr_1_1?keywords=the+alchemist&qid=1686241491&sprefix=the+alceh%2Caps%2C149&sr=8-1",
+  },
+  {
+    id: 3,
+    title: "Bersek",
+    image: "bersek.jpg",
+    stars: null,
+    link: "https://www.amazon.com/Berserk-Deluxe-2-Kentaro-Miura/dp/1506711995/ref=sr_1_5?crid=3TCPQSE8E2X5L&keywords=Bersek&qid=1687096131&sprefix=bersek+%2Caps%2C222&sr=8-5",
+  },
+];
