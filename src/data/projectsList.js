993e22e752aabb20eae7d6528188ad787ebd5243
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-06-18 16:02:54 +0000

    created a data array for the projects List

---
+++ src/data/projectsList.js
@@ -0,0 +1,21 @@
+export const projects = [
+  {
+    id: 1,
+    name: "mariusniemet.me",
+    description: "The source of this website.",
+    demo: null,
+    article: null,
+    source: "https://github.com/niemet0502/mariusniemet.me",
+    image: "flowist.png",
+  },
+  {
+    id: 2,
+    name: "seiri",
+    description:
+      " Seirir is an app that allows you to manage your tasks and notes in the same place for free without switching between multiples products",
+    demo: "https://github.com/niemet0502/Seiri",
+    article: "/how-i-built-a-todo-and-note-app",
+    source: "https://github.com/niemet0502/Seiri",
+    image: "seiri.png",
+  },
+];
