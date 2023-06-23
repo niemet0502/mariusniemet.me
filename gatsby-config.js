571ae4c768dacbef47377edd95a1134304ad211d
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-06-23 22:27:54 +0000

    displayed article by topic on the writings page

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,19 +16,22 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    `gatsby-plugin-mdx`,
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
+        path: `${__dirname}/blog`,
         extensions: [".mdx", "md"],
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/blog/`,
-        name: "blog",
+        name: `blog`,
+        path: `${__dirname}/blog`,
       },
     },
+    `gatsby-transformer-remark`,
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -55,24 +58,6 @@ module.exports = {
       options: {
         name: "assets",
         path: `${__dirname}/static/`,
-      },
-    },
-
-    // animation
-    {
-      resolve: `gatsby-plugin-scroll-reveal`,
-      options: {
-        threshold: 0.5, // Percentage of an element's area that needs to be visible to launch animation
-        once: true, // Defines if animation needs to be launched once
-        disable: false, // Flag for disabling animations
-
-        // Advanced Options
-        selector: "[data-sal]", // Selector of the elements to be animated
-        animateClassName: "sal-animate", // Class name which triggers animation
-        disabledClassName: "sal-disabled", // Class name which defines the disabled state
-        rootMargin: "0% 50%", // Corresponds to root's bounding box margin
-        enterEventName: "sal:in", // Enter event name
-        exitEventName: "sal:out", // Exit event name
       },
     },
   ],
--- src/pages/writings.js
+++ src/pages/writings.js
@@ -1,10 +1,32 @@
 import React from "react";
 
+import { Link, graphql } from "gatsby";
+import { useMemo } from "react";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Layout from "../components/Layout";
 
-const Writings = () => {
+const Writings = ({ data }) => {
+  const posts = data.posts.nodes;
+  const postsbytopic = useMemo(() => {
+    const collection = {};
+
+    for (let i = 0; i < posts.length; i++) {
+      collection[posts[i].frontmatter.categorie] = [];
+      for (let j = 0; j < posts.length; j++) {
+        const { categorie } = posts[j].frontmatter;
+
+        if (posts[i].frontmatter.categorie === categorie) {
+          collection[categorie].push(posts[j]);
+        }
+      }
+    }
+
+    return collection;
+  }, [posts]);
+
+  console.log(postsbytopic);
+
   return (
     <Layout>
       <div className="page-content">
@@ -14,11 +36,46 @@ const Writings = () => {
  also about life in general. You can browse the articles by topic, by date
  or search by keyword below."
         />
+        <h2>Articles by topic</h2>
+        <div className="flex articles">
+          {Object.keys(postsbytopic).map((key) => (
+            <div className="article-container">
+              <h3 className="section-title">{key}</h3>
+
+              {postsbytopic[key].map((post) => (
+                <div className="blog-post" key={post.id}>
+                  <h3>
+                    <Link to={`/${post.frontmatter.slug}`}>
+                      {post.frontmatter.title}
+                    </Link>
+                  </h3>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
 
       <Footer customStyle="footer-alignment" />
     </Layout>
   );
 };
+
+export const blogQuery = graphql`
+  query BlogQuery {
+    posts: allMdx(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        id
+        body
+        frontmatter {
+          title
+          date
+          categorie
+          slug
+        }
+      }
+    }
+  }
+`;
 
 export default Writings;
