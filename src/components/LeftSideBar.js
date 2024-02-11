34cb662a539317596c45b7f81bfffafeaa23315c
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-02-11 22:26:06 +0000

    added social media links

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -6,10 +6,21 @@ const LeftSideBar = () => {
   return (
     <div className="left-nav">
       <div className="">
-        <FaGithubSquare />
-        <BsLinkedin />
-        <FaTwitterSquare />
-        <BsInstagram />
+        <a href="https://github.com/niemet0502" target="_blank">
+          <FaGithubSquare />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/marius-vincent-niemet-928b48182/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        <a href="https://twitter.com/mariusniemet05" target="_blank">
+          <FaTwitterSquare />
+        </a>
+        <a href="" target="_blank">
+          <BsInstagram />
+        </a>
       </div>
     </div>
   );
