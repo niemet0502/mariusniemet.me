54f4edf92281f542be6be2af7e047b0edbf3d6e0
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-03-11 22:58:55 +0000

    added new books to the list

--- src/data/booksList.js
+++ src/data/booksList.js
@@ -20,4 +20,25 @@ export const books = [
     stars: null,
     link: "https://www.amazon.com/Berserk-Deluxe-2-Kentaro-Miura/dp/1506711995/ref=sr_1_5?crid=3TCPQSE8E2X5L&keywords=Bersek&qid=1687096131&sprefix=bersek+%2Caps%2C222&sr=8-5",
   },
+  {
+    id: 4,
+    title: "Distributed systems with Nodejs",
+    image: "distnode-stack.jpg",
+    stars: null,
+    link: "https://www.amazon.com/Distributed-Systems-Node-js-Building-Enterprise-Ready/dp/1492077291/ref=sr_1_1?crid=2QA1S1VUZTBPU&dib=eyJ2IjoiMSJ9.g9nyaIin0MYWnywkQFL0U2FD8SdSmjxzFcFXsflVdi9xcJJdFkaJvbQJcqpccdxuL6220hFPxZWH1a44AhqNWDmEXyOmlkZTuJb71qO8AxPKPbBA0Wx7bvidMeMKtOnjqQfK2Uuwln4oy8jOACwS4S4oKS-K-OouOy6tbPDbSN6zlaSUGXJgTG4sX2O-7j6L-i8m3ApdoEHun0wtUNsap_re4f6-XbpC8_vYdrFTesQ.4YqyORKHvkxj43MKLL7yshSnylyv_dCQG16qjMuCql0&dib_tag=se&keywords=distributed+systems+with+node.js&qid=1710197378&sprefix=distributed+systems+with+%2Caps%2C217&sr=8-1",
+  },
+  {
+    id: 5,
+    title: "Hunger games",
+    image: "hungergames.jpg",
+    stars: null,
+    link: "https://www.amazon.com/Hunger-Hardcover-Catching-Mockingjay-Songbirds/dp/1338686534/ref=sr_1_1?crid=1H279QA20LL78&dib=eyJ2IjoiMSJ9.NmxXdgJtvDNCuRdaSeQKqOI_ZccUvHWkIoRxNX0mC1W3geNd5A2YCKCgHIVXq6kDSqxA_nOnwqONAD8MhA7tZaWg36z4GY_5seU3YvULQQus51rIwWlMi4RxsU8gjrZKTI2BTvIDt94n0vNdj221uch7sycSvxnsg8S_VPF7Jlr-Nvu5ejn4N56QxkyckpglOwKZnzShBFsaC3rTzesInmm8aeCGgRVE4-yOYhN4PLw.f1E5p_WPRzEqQ59FTr4Ak7wo6sIRcVoW4IF0TVPF6G0&dib_tag=se&keywords=hunger+games+books+set+of+4&qid=1710197585&sprefix=hunger+g%2Caps%2C212&sr=8-1",
+  },
+  {
+    id: 6,
+    title: "Le joueur d'echec",
+    image: "lejoureurdechec.jpg",
+    stars: null,
+    link: "https://www.amazon.com/Joueur-d%C3%A9checs-nouvelle-traduction-Litt%C3%A9rature-ebook/dp/B00UOLADRU/ref=sr_1_3?crid=467F5PYRXGX9&dib=eyJ2IjoiMSJ9.m3RSOWgQPOS4tf3Zd9DoVMRM1IJFEHJN4rGJJtvLDSI43NbKOqibKPqC3PI8lMSWK-qo8UxXBSXaLLVtZ7ZIr1dN-ooh55qJNdXEUX30se8GtvnPObuKHdeBJ0veEeuI6lEYBPyB3Ld4Mt29MviMrYEadE7zM8BfwrHvnxFy_IYV1TihJz46H-srmj3Q0EH7J071JG1ZFJvlMbcb_tMunLJJ1mDor4B5excuG1XiY24.6iJvq-XqhsANg2y0Kws5jH_2PjbgFu4u-7Yvgr56VbQ&dib_tag=se&keywords=le+joueur+d%27echecs+stefan+zweig&qid=1710197398&sprefix=Le+joueur+%2Caps%2C225&sr=8-3",
+  },
 ];
