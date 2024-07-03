26461e3398353a0c6d52471b3afcc0c18549d2ef
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-07-03 10:23:29 +0000

    added guma as project

--- src/data/projectsList.js
+++ src/data/projectsList.js
@@ -13,11 +13,22 @@ export const projects = [
     id: 2,
     name: "seiri",
     description:
-      " Seirir is an app that allows you to manage your tasks and notes in the same place for free without switching between multiples products",
+      " Seiri is an app that allows you to manage your tasks and notes in the same place for free without switching between multiples products",
     demo: "https://seiri.mariusniemet.me/",
     article: "/building-seiri-my-tasks-notes-app",
     source: "https://github.com/niemet0502/Seiri",
     image: "seiri.png",
+    year: 2023,
+  },
+  {
+    id: 3,
+    name: "Guma",
+    description:
+      "Guma is a Lightweight project management app Built with Microservices architecture, GraphQL Federation, React and Observability.",
+    demo: "https://guma.mariusniemet.me/",
+    article: null,
+    source: "https://github.com/niemet0502/guma",
+    image: "guma.png",
     year: 2023,
   },
 ];
