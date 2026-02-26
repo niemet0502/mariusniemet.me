e9dd7a8e76433b8042f6b84d09c4ed1f6ba2a62e
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2026-02-26 14:21:16 +0000

    feat: few design improvement

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,4 +1,4 @@
-import { graphql, Link } from "gatsby";
+import { graphql } from "gatsby";
 import React from "react";
 import { Helmet } from "react-helmet";
 import Layout from "../components/Layout";
@@ -17,7 +17,7 @@ export default function PostTemplate({ data }) {
 
       <div className="page-content">
         <div className="blog-post-container">
-          <div className="flex-1 post-content">
+          <div className="post-content">
             <div className="post-content__head">
               <h1>{frontmatter.title}</h1>
             </div>
@@ -27,16 +27,6 @@ export default function PostTemplate({ data }) {
               className="post-content__body"
               dangerouslySetInnerHTML={{ __html: html }}
             />
-          </div>
-          <div className="right-sidebar">
-            <h2>About me</h2>
-
-            <p>
-              Hello and thanks for visiting!. I'm{" "}
-              <Link to="/about">Marius</Link> <br />a software Engineer and
-              Writer based in Dakar. I like building stuff and writing about
-              them.
-            </p>
           </div>
         </div>
       </div>
